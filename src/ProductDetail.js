504a4bfd8e6474d69e36fc4bfb0a15a9eb53c279
Jay Ajmera <ajmerajay.ja@gmail.com>
2023-10-11 17:02:54 +0530

    Dynamic

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -1,35 +1,30 @@
-
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
 const products = [
   { id: 1, name: 'Product 1', price: 10, description: 'Description for Product 1' },
   { id: 2, name: 'Product 2', price: 15, description: 'Description for Product 2' },
   { id: 3, name: 'Product 3', price: 20, description: 'Description for Product 3' },
+  { id: 4, name: 'Product 8', price: 20, description: 'Description for Product 3' },
 ];
 
-
-function ProductDetail({ addToCart }) {
-  const { id } = useParams();
-  const productId = parseInt(id);
-  const product = products.find(product => product.id === productId);
-
-  if (!product) {
-    return <div>Product not found!</div>;
-  }
-
-  const handleAddToCart = () => {
+function Home({ addToCart }) {
+  const handleAddToCart = (product) => {
     addToCart(product);
     alert('Product added to cart!');
   };
 
   return (
-    <div>
-      <h1>{product.name}</h1>
-      <p>${product.price}</p>
-      <button onClick={handleAddToCart}>Add to Cart</button>
+    <div className="product-list">
+      {products.map(product => (
+        <div className="product-card" key={product.id}>
+          <h2>{product.name}</h2>
+          <p>${product.price}</p>
+          <p>{product.description}</p>
+          <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default ProductDetail;
+export default Home;
